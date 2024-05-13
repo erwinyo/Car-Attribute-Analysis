23149f9bab7b9f30ab810ca493caa93b345b3ee1
Erwin Yonata <86058013+yokahealthcare@users.noreply.github.com>
2024-05-14 01:51:51 +0700

    upload setup file

--- setup-package.sh
+++ setup-package.sh
@@ -2,6 +2,12 @@
 
 pip install -r requirements.txt
 
+# Your machine has CUDA9 or CUDA10 installed, run the following command to install it
+# I'm running on CUDA 12.x is OK
+# There is also CPU-ONLY this is the documentation for more details
+# https://github.com/PaddlePaddle/PaddleClas/blob/release/2.5/docs/zh_CN/models/PULC/PULC_vehicle_attribute.md
+pip install paddlepaddle-gpu -i https://mirror.baidu.com/pypi/simple
+
 # onnxruntime-gpu for CUDA version 12.x
 # You can refer to this documentation for installing onnxruntime-gpu
 # https://onnxruntime.ai/docs/install/#install-onnx-runtime-ort-1
